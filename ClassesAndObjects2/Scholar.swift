f9654cdcb71981fdaa7c4ee2b1401cecd10c772f
brandigehrke <brandi.f.gehrke@gmail.com>
2020-08-08 21:03:50 -0400

    Initial Commit

---
+++ ClassesAndObjects2/Scholar.swift
@@ -0,0 +1,9 @@
+//
+//  Scholar.swift
+//  ClassesAndObjects2
+//
+//  Created by Brandi Freeman on 7/10/20.
+//  Copyright © 2020 Brandi Freeman. All rights reserved.
+//
+
+import Foundation
---
+++ ClassesAndObjects2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassesAndObjects2
+//
+//  Created by Brandi Freeman on 7/10/20.
+//  Copyright © 2020 Brandi Freeman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
